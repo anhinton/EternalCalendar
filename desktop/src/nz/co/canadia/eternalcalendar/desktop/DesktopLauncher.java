3f03e72c0b23e0669234206f922f598b8c71a2a6
Ashley Noel Hinton <ashley@canadia.co.nz>
2021-05-03 10:40:15 +1200

    Initial commit

---
+++ desktop/src/nz/co/canadia/eternalcalendar/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package nz.co.canadia.eternalcalendar.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import nz.co.canadia.eternalcalendar.EternalCalendar;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new EternalCalendar(), config);
+	}
+}
