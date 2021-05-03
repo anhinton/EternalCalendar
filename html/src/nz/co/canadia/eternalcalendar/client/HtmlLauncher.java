8161e923d5b033ccd7cfbf92564b0ac9848f53b9
Ashley Noel Hinton <ashley@canadia.co.nz>
2021-05-03 14:32:38 +1200

    Set Html dimension

--- html/src/nz/co/canadia/eternalcalendar/client/HtmlLauncher.java
+++ html/src/nz/co/canadia/eternalcalendar/client/HtmlLauncher.java
@@ -3,16 +3,15 @@ package nz.co.canadia.eternalcalendar.client;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+
+import nz.co.canadia.eternalcalendar.Constants;
 import nz.co.canadia.eternalcalendar.EternalCalendar;
 
 public class HtmlLauncher extends GwtApplication {
 
         @Override
         public GwtApplicationConfiguration getConfig () {
-                // Resizable application, uses available space in browser
-                return new GwtApplicationConfiguration(true);
-                // Fixed size application:
-                //return new GwtApplicationConfiguration(480, 320);
+                return new GwtApplicationConfiguration(Constants.GAME_WIDTH, Constants.GAME_HEIGHT);
         }
 
         @Override
